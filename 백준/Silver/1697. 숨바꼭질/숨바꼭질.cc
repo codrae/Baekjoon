4317cee4e28a9cc372d39e6919e2965b91eb2df7
Yongjin <hi653@naver.com>
2024-02-01 09:42:22 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,4 +1,31 @@
 #include <iostream>
+#include <algorithm>
+using namespace std;
+
+int c(int p, int d) {
+    // 동생이 더 뒤,혹은 같이 있는 경우
+    if (p <= d)
+        return d - p;
+    // 남은 모든 경우에 대해서 수빈이는 동생보다 앞에 있다.
+    // p == 1 ? 수빈이는(N) == 0
+    else if (p == 1) return 1;
+    // p가 홀수인 경우 !! +-1 거리중 최솟값 (재귀)
+    // 도착지가 홀수인 경우 *2로는 도착 불가능. 결국 한번은 +-1 중 하나 수행하여야 함.
+    else if (p % 2) return 1 + min(c(p - 1, d), c(p + 1, d));
+    // p가 짝수인경우 !! 직접 이동하는 경우와 *2 중 최솟값 비교
+    else return min(p - d, 1 + c(p / 2, d));
+}
+
+int main() {
+
+    int N, K;
+
+    cin >> N >> K;
+    cout << c(K,N);
+}
+
+/*
+#include <iostream>
 #include <queue>
 using namespace std;
 
@@ -44,3 +71,5 @@ int main() {
 
     return 0;
 }
+
+ */

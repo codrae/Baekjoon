8bf5cef21bf2a22235efc9c75d33fe44f5d1c6e1
Yongjin <hi653@naver.com>
2024-02-04 15:02:18 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 
-int L,H;
+unsigned long long L,H,x;
 string s;
 #define r 31
 #define M 1234567891
@@ -11,10 +11,18 @@ int main() {
     cin >> L;
     cin >> s;
     for (int i = 0; i < L ; ++i) {
-        H += ((s[i] - 96)*int(pow(r,i)));
+        x = s[i] - 96;
+        for (int j = 0; j < i; ++j) {
+            x *= r;
+            x %= M;
+        }
+        H += x;
+        H %= M;
     }
 
-    cout << H%M;
+    cout << H << endl;
 
     return 0;
 }
+
+//https://velog.io/@gidskql6671/%EB%82%98%EB%A8%B8%EC%A7%80Modulo-%EC%97%B0%EC%82%B0-%EB%B6%84%EB%B0%B0%EB%B2%95%EC%B9%99
